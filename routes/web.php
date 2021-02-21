80b1e0bbe8d0b8f140ae0f7fd7b0ff411f5a990e
Yurinabaia <yurinabaiaduarte@gmail.com>
2021-02-21 15:22:51 -0300

    rotas sigilosas, o usuario precisar autentica para entra nessa rota Route::prefix('/app')->group(function () {//rotas});

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,17 @@ Route::get('/', 'PrincipalController@principal');//O Laravel identifica isso com
 //Para criar um controller use o codigo
 //O @ significa o nome da Action que vamos executar dentro do controller
 //codigo para o controller: php artisan make:controller PrincipalController 
-
-
 Route::get('/sobrenos', 'SobreNosController@sobrenos');
 Route::get('/contatos', 'ContatosController@contatos');
+Route::get('/login', function() { return "login";});
+
 
 //Para testa saber as rotas configuradas usar o php artisan route:list
+
+//Definido rotas sigilosas, onde o usuario precisa se autentica para entra na rota
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function() {return "clientes";});
+    Route::get('/fornecedores', function() {return "fornecedores";});
+    Route::get('/produtos', function() {return ("produtos");});
+});
 
