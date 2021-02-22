591497c89979f71d473fcd4649d90ecead25af4b
Yurinabaia <yurinabaiaduarte@gmail.com>
2021-02-22 20:04:42 -0300

    Usando for no blade

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -10,7 +10,7 @@ class FornecedoresController extends Controller
     public function index() 
     {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.00'],
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.00', 'ddd' => '11', 'tel' => '000-000-000'],
             1 => ['nome' => 'Fornecedor 2', 'status' => null, 'ddd' => '11', 'tel' => '000-000-000' ],
             2 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.00', 'ddd' => '31', 'tel' => '000-000-000'],
             3 => ['nome' => 'Fornecedor 2', 'status' => null, 'ddd' => '21', 'tel' => '000-000-000']
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -15,27 +15,31 @@ echo 'teste';
 @endphp
 
 
+
+@for ($i = 0; $i < count($fornecedores); $i++)
+
 <!--@ dd($fornecedores) Printa array no navegador, apenas tire o espasso deixado @ junto com dd -->
 @isset($fornecedores)<!--Se a variavel for existe ele entra nessa condição, caso contraio não entra e nao dar erro -->
-    <br>
-    Fornecedores: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    Cnpj: {{$fornecedores[1]['status'] ?? 'Dado não preenchido' }}
+        <br>
+        Fornecedores: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        Cnpj: {{ $fornecedores[$i]['status'] ?? 'Dado não preenchido' }}
 
-    Telefone: ({{$fornecedores[1]['ddd'] ?? ''}}) ({{$fornecedores[1]['tel'] ?? ''}})
-    @switch($fornecedores[1]['ddd'])
-        @case('11')
-            <h3> São paulo </h3>
-            @break
-        @case('21')
-            <h3> Minas Gerais
-            @break
-        @case('31')
-            <h3> Rio de janeiro
-            @break
-        @default
-            <h3> Estado não encontrado </h3>
-    @endswitch
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) ({{ $fornecedores[$i]['tel'] ?? '' }})
+        @switch($fornecedores[$i]['ddd'])
+                @case('11')
+                    <h3> São paulo </h3>
+                    @break
+                @case('21')
+                    <h3> Minas Gerais
+                    @break
+                @case('31')
+                    <h3> Rio de janeiro
+                    @break
+                @default
+                    <h3> Estado não encontrado </h3>
+        @endswitch
 @endisset
+@endfor
