10a4572170044f88a7942a926c117cd123d02ea4
Yurinabaia <yurinabaiaduarte@gmail.com>
2021-02-24 19:16:35 -0300

    Criei a pasta css e criei uma layout pelo laravel usando section e yield

---
+++ resources/views/site/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+<body>
+    @yield('conteudoIndex') <!--Buscando do index para a secao conteudo que ele traz -->
+    @yield('conteudoAbout')<!--Buscando do about para a secao conteudo que ele traz -->
+    @yield('conteudoContatos')<!--Buscando do about para a secao conteudo que ele traz -->
+</body>
+</html>
